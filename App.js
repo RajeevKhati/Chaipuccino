e7ee8f9bf2f2c71260935b330fe4362e5248751a
Rajeev Khati <rajeev.khati@nagarro.com>
2022-08-23 17:27:52 +0530

    add splash screen

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {GlobalProvider} from './src/context/Provider';
 import AppContainer from './src/routes';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
       <GlobalProvider>
